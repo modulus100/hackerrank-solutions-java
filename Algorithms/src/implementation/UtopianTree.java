c1ba7be1e519d51ae631a16670cab7e1e57b64aa
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-12-26 23:21:48 +0200

    utopian tree

--- Algorithms/src/implementation/UtopianTree.java
+++ Algorithms/src/implementation/UtopianTree.java
@@ -4,9 +4,7 @@ public class UtopianTree {
 
     static int utopianTree(int n) {
         if (n < 3) return n + 1;
-        if (n % 2 == 0)
-            return (utopianTree(n - 2) * 2) + 1;
-        else
-            return (utopianTree(n - 2) + 1) * 2;
+        if (n % 2 == 0) return (utopianTree(n - 2) * 2) + 1;
+        return (utopianTree(n - 2) + 1) * 2;
     }
 }
