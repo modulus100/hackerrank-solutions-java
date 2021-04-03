e5c142a7b711a9ab7c3e3b2756483e596154da31
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-03 16:33:03 +0300

    min abs diff

--- Algorithms/src/greedy/MinimumAbsoluteDifferenceInArray.kt
+++ Algorithms/src/greedy/MinimumAbsoluteDifferenceInArray.kt
@@ -1,9 +1,18 @@
 package greedy
 
+import kotlin.math.abs
+
 fun main(args: Array<String>) {
     print(minimumAbsoluteDifference(arrayOf()))
 }
 
 fun minimumAbsoluteDifference(arr: Array<Int>): Int {
-    return 10;
+    arr.sort()
+    var minAbs = Int.MAX_VALUE
+
+    for (i in 0..arr.size - 2) {
+        minAbs = minAbs.coerceAtMost(abs(arr[i] - arr[i + 1]))
+    }
+
+    return minAbs;
 }
