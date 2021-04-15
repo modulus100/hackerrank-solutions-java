cee5b67883bf4aa9b2a0f72acf2c6ae4590d64c1
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2021-04-16 00:58:45 +0300

    greedy florist

--- InterviewPreparationKit/src/greedy/algorithms/GreedyFlorist.kt
+++ InterviewPreparationKit/src/greedy/algorithms/GreedyFlorist.kt
@@ -1,10 +1,22 @@
 package greedy.algorithms
 
 fun main(args: Array<String>) {
-
+    val result = getMinimumCost(3, arrayOf(1, 3, 5, 7, 9))
+    print(result)
 }
 
-/*
 fun getMinimumCost(k: Int, c: Array<Int>): Int {
+    var cost = 0
+    var i = 0
 
-}*/
+    c.sortDescending()
+    c.forEachIndexed { index, element ->
+        cost += (i + 1) * element
+
+        if ((index + 1) % k == 0) {
+            i++
+        }
+    }
+
+    return cost
+}
