c88eddcc24ee95372238856aef86918ffe3e7b61
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2022-08-05 22:23:43 +0300

    mini max

--- Algorithms/src/recursion/SuperDigit.java
+++ Algorithms/src/recursion/SuperDigit.java
@@ -14,25 +14,13 @@ public class SuperDigit {
         return superDigit(str, 1);
     }
 
-    static int superDigit2(String number, int k) {
-        if (number.length() > 1) {
-            long sum = 0;
-            for (int i = 0; i < number.length(); i++) {
-                sum += Character.getNumericValue(number.charAt(i));
-            }
-            return superDigit2(Long.toString(sum * k), 1);
-        }
-
-        return Character.getNumericValue(number.charAt(0));
-    }
-
-    static int superDigit3(String n, int k) {
+    static int superDigit2(String n, int k) {
         if (n.length() > 1) {
             var sum = Arrays.stream(n.split(""))
                     .map(Long::parseLong)
                     .reduce(0L, Long::sum);
 
-            return superDigit3(Long.toString(sum * k), 1);
+            return superDigit2(Long.toString(sum * k), 1);
         }
 
         return Character.getNumericValue(n.charAt(0));
