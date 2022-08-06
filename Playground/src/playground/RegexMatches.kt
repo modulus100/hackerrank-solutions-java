6e6e9b3b53f3fc170150a41401ba82e5fcf14a52
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2022-08-06 14:50:44 +0300

    rename modules

--- Playground/src/playground/RegexMatches.kt
+++
@@ -1,7 +0,0 @@
-package playground
-fun main() {
-    val test = "test, .01"
-    println(matches(test))
-}
-
-fun matches(str: String) = "[A-Za-z0-9,. ]+".toRegex().matches(str)
--- Playground/src/playground/binary/tree/MaxBinaryTreeSum.kt
+++
@@ -1,2 +0,0 @@
-package playground.binary.tree
-
